abc46f022d982fa703324cad2a5fcaf7d8eb7773
Darby Yule <darby@theyulebrothers.com>
2022-04-30 12:15:41 -0700

    ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while(i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+
+const bananabread = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for(let i=0; i<bananabread.length; i++){
+  console.log(bananabread[i]);
+}
+
+const banana = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for(let i=banana.length - 1; i >= 0; i--){
+  console.log(banana[i]);
+}
